35d180b74fe76f8bb3eb7c37a4988271d26d048e
Alians <72793056+x20000826@users.noreply.github.com>
2024-04-17 22:43:50 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     return [
       {
         source: "/proxy/:slug*",
-        destination: "https://api.openai.com/:slug*",
+        destination: "https://api.chatanywhere.com.cn/:slug*",
       },
       {
         source: "/proxy-sse/:slug*",
